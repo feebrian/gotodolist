d23449aed61ad70943885a85db0f4169c23992ff
feebrian <adamaditama13@gmail.com>
2023-07-07 10:40:52 +0700

    update envrc and set cors

--- api/route.go
+++ api/route.go
@@ -3,9 +3,12 @@ package api
 import (
 	"github.com/go-layer-architecture/internal/app/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func Router(app *fiber.App) {
+	app.Use(cors.New(cors.ConfigDefault))
+
 	app.Post("/user", handlers.CreateUser)
 	app.Get("/users", handlers.FindUsers)
 	app.Get("/users/:id", handlers.GetUser)
